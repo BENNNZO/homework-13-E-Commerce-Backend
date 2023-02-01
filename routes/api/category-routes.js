5819fc9369085c1bab399ef425cd1aab12650e6f
Benjamin Phillips <benjamingt537@gmail.com>
2023-01-31 19:29:36 -0500

    finnaly got things working but all models should be working and category routs aswell

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,65 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: { model: Product }
+  })
+  .then(data => res.json(data))
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk(req.params.id, {
+    include: { model: Product }
+  })
+  .then(category => res.json(category))
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+  .then((category) => {
+    res.json(category)
+  })
+  .catch((err) => res.json(err))
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+  .then(updatedData => res.json(updatedData))
+  .catch(err => console.log(err))
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No library card found with that id!' })
+      return;
+    }
+
+    res.status(200).json(categoryData)
+  } 
+  catch (err) { 
+    res.status(500).json(err) 
+  }
 });
 
 module.exports = router;
